380fdd1cf11a18697963e8fdb87478bb4b57592e
ndom91 <yo@ndo.dev>
2020-09-04 15:25:39 +0200

    update: refactor to use pools better

--- index.js
+++ index.js
@@ -1,29 +1,43 @@
-require('dotenv').config()
-const sql = require('mssql')
-const countries = require('./countries.json')
+require("dotenv").config();
+const sql = require("mssql");
+const countries = require("./countries.json");
 
 const config = {
-  host: process.env.MYSQL_HOST,
-  database: process.env.MYSQL_DATABASE,
-  port: process.env.MYSQL_PORT,
-  user: process.env.MYSQL_USER,
-  password: process.env.MYSQL_PASSWORD
+	server: process.env.MYSQL_HOST,
+	database: process.env.MYSQL_DATABASE,
+	port: parseInt(process.env.MYSQL_PORT, 10),
+	user: process.env.MYSQL_USER,
+	password: process.env.MYSQL_PASSWORD
+};
+
+const pool = new sql.ConnectionPool(config);
+const poolConnect = pool.connect();
+
+pool.on('error', err => {
+	console.error('Err: ', err)
+})
+
+async function writeCountry() {
+	await poolConnect;
+	try {
+		const request = pool.request();
+		const result = await request.query(`update allg_laender set land_lang = '${country.name.toUpperCase()}' where land_kurz like '${country.alpha2.toUpperCase()}'`)
+		if (result.rowsAffected > 0) {
+			console.log(`Successfully updated - ${country.name}`);
+		} else {
+			console.error("No rows affected!");
+		}
+	} catch (err) {
+		console.error('SQL error', err.originalError.info.message);
+	}
 }
 
-const queryLoop = async () => {
-  await countries.forEach(country => {
-    new sql.ConnectionPool(`mssql://${config.user}:${config.password}@${config.host}:${config.port}/${config.database}`).connect().then(connection => {
-      return connection.query`update allg_laender set land_lang = ${country.name.toUpperCase()} where land_kurz = ${country.alpha2.toUpperCase()}`
-    }).then(result => {
-      if (result.rowsAffected > 0) {
-        console.log(`Successfully updated - ${country.name}`)
-      } else {
-        console.error('No rows affected!')
-      }
-    }).catch(err => {
-      console.error(err)
-    })
-  })
+const main = async () => {
+	for await (country of countries) {
+		writeCountry(country)
+	}
 }
 
-queryLoop()
+// Workaround for missing top-level await
+main()
+
